d24938854ca7d076bae078d0fcc05c1cb84e9e9b
hp <hp@DESKTOP-TM0OM7E.mshome.net>
2025-03-05 10:08:11 +0530

    TestScript

---
+++ ActiTime/src/com/actitime/generic/BaseClass.java
@@ -0,0 +1,49 @@
+package com.actitime.generic;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.actime.pom.HomePage;
+import com.actime.pom.LoginPage;
+
+public class BaseClass {
+	public static WebDriver driver;
+	@BeforeTest
+	public void openBrowser() {
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+	@BeforeMethod
+	public void login() throws IOException {
+		FileLib f=new FileLib();
+		String url = f.getPropertydata("url");
+		String un = f.getPropertydata("uaername");
+		String pwd = f.getPropertydata("password");
+		driver.get(url);
+		LoginPage l=new LoginPage(driver);
+		
+	}
+	
+	@AfterMethod
+	public void logout() {
+		HomePage h=new HomePage(driver);
+		h.setLogOut();
+	}
+	
+
+
+}
